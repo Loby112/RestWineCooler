a5187802a2e7737f04abe7612f7cbb76fccc4f73
Tobias <tobi195h@edu.easj.dk>
2022-06-05 14:49:53 +0200

    Add project files.

---
+++ RestWineCooler/Controllers/WineCoolersController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using RestWineCooler.Managers;
+using WineCoolerLib;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestWineCooler.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WineCoolersController : ControllerBase{
+        private WineCoolersManager manager = new WineCoolersManager();
+        // GET: api/<WineCoolersController>
+        [HttpGet]
+        public ActionResult<IEnumerable<WineCooler>> Get(){
+            return Ok(manager.GetAllCoolers());
+        }
+
+        // GET api/<WineCoolersController>/5
+        [HttpGet("{id}")]
+        public ActionResult<WineCooler> Get(int id){
+            WineCooler result = manager.GetCooler(id);
+            if (result == null){
+                return NotFound(id);
+            }
+            return Ok(result);
+        }
+
+        // POST api/<WineCoolersController> 
+        [HttpPost]
+        public ActionResult<WineCooler> Post([FromBody] WineCooler newCooler) {
+            if (newCooler.BottlesInStorage < 0 || newCooler.CapacityOfBottles <= 0){
+                return BadRequest(newCooler);
+            }
+
+            manager.AddCooler(newCooler);
+            
+
+
+
+            return Created("api/winecoolers/" + newCooler.Id, newCooler);
+        }
+
+
+        // DELETE api/<WineCoolersController>/5
+        [HttpDelete("{id}")]
+        public ActionResult<WineCooler> Delete(int id){
+            WineCooler result = manager.GetCooler(id);
+            if (result == null){
+                return NotFound(id);
+            }
+
+            return Ok(manager.DeleteCooler(id));
+        }
+
+        [HttpGet("{id}/Addwine")]
+        public ActionResult<bool> AddWine([FromRoute] int id){
+            var result = manager.GetCooler(id);
+            if(result.CoolerIsFull()){
+                return Conflict();
+            }
+
+            return Ok("Bottles now " + manager.AddWine(id));
+
+        }
+
+    }
+}
---
+++ RestWineCooler/Managers/WineCoolersManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication;
+using WineCoolerLib;
+
+namespace RestWineCooler.Managers {
+    public class WineCoolersManager{
+        private static int nextId = 1;
+
+        private static List<WineCooler> coolersList = new List<WineCooler>(){
+            new WineCooler(nextId++, 4, 18, 4),
+            new WineCooler(nextId++, 4, 12, 0),
+            new WineCooler(nextId++, 3, 12, 1),
+            new WineCooler(nextId++, 48, 10, 44),
+        };
+
+        public IEnumerable<WineCooler> GetAllCoolers(){
+            return coolersList;
+        }
+
+        public WineCooler GetCooler(int id){
+            var result = coolersList.Find(C => C.Id == id);
+            return result;
+        }
+
+        public WineCooler AddCooler(WineCooler cooler){
+            cooler.Id = nextId++;
+            coolersList.Add(cooler);
+            return cooler;
+        }
+
+        public WineCooler DeleteCooler(int id){
+            WineCooler result = coolersList.Find(C => C.Id == id);
+            coolersList.Remove(result);
+            return result;
+        }
+
+        public bool AddWine(int id){
+            var result = coolersList.Find(C => C.Id == id);
+            if (!result.CoolerIsFull()){
+                result.AddWine();
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
